c4b260d3fcf4b6f4b9419805eaa1e11a8d9730a6
hossein <mdoroud97@gmail.com>
2022-10-18 22:19:50 +0330

    feat(Loading): add loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Loading = () => <Container>Loading...</Container>;
+
+export default Loading;
