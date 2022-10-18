ef64d40cefd3ecbd996d919926ed43aaf6f42cbc
hossein <mdoroud97@gmail.com>
2022-10-18 22:50:44 +0330

    chore: remove proxy

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-module.exports = function (app) {
-  app.use(
-    createProxyMiddleware(["/fa/v1"], {
-      target: "http://api.aparat.com",
-    })
-  );
-};
